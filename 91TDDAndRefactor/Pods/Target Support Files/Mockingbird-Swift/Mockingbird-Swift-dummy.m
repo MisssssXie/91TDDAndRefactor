bc8ccaf60be133c2d43b0dd2e04f21a9f298c5cc
Sally <nt92095@cathaybk.com.tw>
2023-08-23 08:29:42 +0800

    chore: pod init and add MockingBird

---
+++ 91TDDAndRefactor/Pods/Target Support Files/Mockingbird-Swift/Mockingbird-Swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Mockingbird_Swift : NSObject
+@end
+@implementation PodsDummy_Mockingbird_Swift
+@end
---
+++ 91TDDAndRefactor/Pods/Target Support Files/Pods-91TDDAndRefactor-91TDDAndRefactorUITests/Pods-91TDDAndRefactor-91TDDAndRefactorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_91TDDAndRefactor_91TDDAndRefactorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_91TDDAndRefactor_91TDDAndRefactorUITests
+@end
