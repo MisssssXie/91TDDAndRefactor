0a7709442b77d5189979ab50d1e78e81298190a5
Sally <nt92095@cathaybk.com.tw>
2023-08-23 09:14:30 +0800

    chore: add 新專案

--- 91TDDAndRefactor/Pods/Target Support Files/Mockingbird-Swift/Mockingbird-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Mockingbird_Swift : NSObject
-@end
-@implementation PodsDummy_Mockingbird_Swift
-@end
--- 91TDDAndRefactor/Pods/Target Support Files/Pods-91TDDAndRefactor-91TDDAndRefactorUITests/Pods-91TDDAndRefactor-91TDDAndRefactorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_91TDDAndRefactor_91TDDAndRefactorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_91TDDAndRefactor_91TDDAndRefactorUITests
-@end
